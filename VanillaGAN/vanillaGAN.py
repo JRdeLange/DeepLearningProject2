184fd65c9b0cc74bd8e2afec11dcf4ca219ad4d4
EKCollins <e.k.collins@student.rug.nl>
2021-04-01 22:01:52 +0200

    vanilla almost works

--- VanillaGAN/vanillaGAN.py
+++ VanillaGAN/vanillaGAN.py
@@ -1,152 +1,198 @@
-
-import torch 
-import torchvision 
-import torch.nn as nn
-import torch.nn.functional as functional
-import numpy as np 
-import datetime
-import os, sys
-
-from matplotlib.pyplot import imshow, imsave
-
+from __future__ import print_function
+import torch
+from torch import nn 
+import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from torchvision.utils import save_image
+import argparse
+import os
+import random
+#import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+from time import time 
 
-# website: https://github.com/Yangyangii/GAN-Tutorial/blob/master/MNIST/VanillaGAN.ipynb
+image_size = 128
 
-MODEL_NAME = 'Vanilla_GAN'
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu" )
+epochs = 1
 
-class Discriminator(nn.Module):
+batch_size = 32
 
-	def __init__(self, input_size = 784, num_classes = 1):
-		super(Discriminator, self).__init__()
-		self.layer = nn.Sequential(
-			nn.Linear(input_size, 512),
-			nn.LeakyReLU(0.2),
-			nn.Linear(512, 256),
-			nn.LeakyReLU(0.2),
-			nn.Linear(256, num_classes),
-			nn.Sigmoid()#,
-			)
-	def forward(self, x):
-		y_ = x.view(x.size(0), -1)
-		y_ = self.layer(y_)
-		return y_
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class Generator(nn.Module):
-	def __init__(self, input_size = 100, num_classes = 784):
-		super(Generator, self).__init__()
-
-		self.layer = nn.Sequential(
-			nn.Linear(input_size, 128),
-			nn.LeakyReLU(0.2),
-			nn.Linear(128, 256),
-			nn.BatchNorm1d(256),
-			nn.LeakyReLU(0.2),
-			nn.Linear(256, 512),
-			nn.BatchNorm1d(512),
-			nn.LeakyReLU(0.2),
-			nn.Linear(512, 1024),
-			nn.BatchNorm1d(1024),
-			nn.LeakyReLU(0.2),
-			nn.Linear(1024, num_classes),
-			nn.Tanh()
-			)
-
-	def forward(self, x):
-		y_ = self.layer(x)
-		#y_ = y_.view(x.size(0), 1, 28, 28)
-		return y_
-
-def get_sample_image(G, n_noise):
-
-	z = torch.randn(100, n_noise).to(DEVICE)
-	y_hat = G(z).view(100, 28, 28) # (100,28,28)
-	results = y_hat.cpu().data.numpy()
-	img = np.zeros([280,280])
-	for j in range(10):
-		img[j*28:(j+1)*28] = np.concatenate([x for x in result[j*10:(j_1)*10]], axis=-1)
-	return img
-
-n_noise = 100
-
-D = Discriminator().to(DEVICE)
-G = Generator(n_noise).to(DEVICE)
-
-img_size = 224
+# data 
+root_path = "archive/front/"
 
 transform = transforms.Compose([
-	transforms.Resize((256,256)),
+	transforms.Resize((image_size, image_size)),
 	transforms.ToTensor(),
 	transforms.Normalize(mean=[0.5], std=[0.5])]
 	)
 
-root_path = "archive/front/"
 data = datasets.ImageFolder(root = root_path, transform=transform)
-batch_size = 64
 data_loader = DataLoader(dataset = data, batch_size = batch_size, shuffle = True, drop_last = False)
 
-criterion = nn.BCELoss()
-D_opt = torch.optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
-G_opt = torch.optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+#---------------------------------------------------------
+#---------------------------------------------------------
+#---------------------------------------------------------
 
-max_epoch = 15
-step = 0
-n_critic = 1
+class Generator(nn.Module):
+	def __init__(self, latent_dim):
+		super(Generator, self).__init__()
+		output_dim = image_size * image_size * 3
+		self.latent_dim = latent_dim 
+		
+		self.main = nn.Sequential(
+			nn.Linear(latent_dim, 256),
+			nn.LeakyReLU(),
 
-D_labels = torch.ones(batch_size, 1).to(DEVICE)
-D_fakes = torch.zeros(batch_size, 1).to(DEVICE)
+			nn.Linear(256, 512),
+			nn.LeakyReLU(),
 
-if not os.path.exists('samples'):
-	os.makedirs('samples')
+			nn.Linear(512, 1024),
+			nn.LeakyReLU(),
 
-for epoch in range(max_epoch):
-	for idx, (images, _) in enumerate(data_loader):
-		# train D
-		x = images.to(DEVICE)
-		x_outputs = D(x)
-		D_x_loss = criterion(x_outputs, D_labels)
+			nn.Linear(1024, output_dim),
+			nn.Tanh()
+			)
+	def forward(self, input_tensor):
+		input_tensor = input_tensor.view(input_tensor.size(0), -1)
+		intermediate = self.main(input_tensor)
+		return intermediate
 
-		z = torch.randn(batch_size, n_noise).to(DEVICE)
-		z_outputs = D(G(z))
-		D_z_loss = criterion(z_outputs, D_fakes)
-		D_loss = D_x_loss + D_z_loss
+#---------------------------------------------------------
+#---------------------------------------------------------
 
-		D.zero_grad()
-		D_loss.backward()
-		D_opt.step()
+class  Discriminator(nn.Module):
+	def __init__(self):
+		super(Discriminator, self).__init__()
 
-		if step % n_critic == 0:
-			# train G
-			z = torch.randn(batch_size, n_noise).to(DEVICE)
-			z_outputs = D(G(z))
-			G_loss = criterion(z_outputs, D_labels)
-
-			G.zero_grad()
-			G_loss.backward()
-			G_opt.step()
-
-		if step % 500 == 0:
-			print('Epoch: {}/{}, Step: {}, D Loss: {}, G Loss: {}'.format(epoch, max_epoch, step, D_loss.item(), G_loss.item()))
-
-		if step % 1000 == 0:
-			G.eval()
-			img = get_sample_image(G, n_noise)
-			imsave('samples/{}_step{}.jpg'.format(MODEL_NAME, str(step).zfill(3)), img, cmap='gray')
-			G.train()
-
-		step += 1
-
-
-G.eval()
-imshow(get_Sample_image(G, n_noise), cmap='gray')
-
-torch.save(D.state_dict(), 'D.pkl')
-torch.save(G.state_dict(), 'G.pkl')
+		input_dim = image_size * image_size * 3
+		output_dim = 1
+		self.main = nn.Sequential(
+			nn.Linear(input_dim, 1024),
+			nn.LeakyReLU(),
+			nn.Dropout(0.3),
+						
+			nn.Linear(1024, 512),
+			nn.LeakyReLU(),
+			nn.Dropout(0.3),
+						
+			nn.Linear(512, 256),
+			nn.LeakyReLU(),
+			nn.Dropout(0.3),
+						
+			nn.Linear(256, output_dim),
+			nn.Sigmoid()
+			)
 
 
+	def forward(self, input_tensor):
+		input_tensor = input_tensor.view(batch_size, -1)
+		intermediate = self.main(input_tensor)
+		return intermediate
+
+#---------------------------------------------------------
+#---------------------------------------------------------
+#---------------------------------------------------------
 
 
+class VanillaGAN():
+	def __init__(self, generator, discriminator, lr_d=1e-3, lr_g=2e-4):
+		self.generator = generator.to(DEVICE)
+		self.discriminator = discriminator.to(DEVICE)
+		self.criterion = nn.BCELoss()
+		self.optim_d = optim.Adam(discriminator.parameters(), lr = lr_d, betas=(0.5, 0.999))
+		self.optim_g = optim.Adam(generator.parameters(), lr = lr_g, betas=(0.5,0.999))
+		self.target_ones = torch.ones((batch_size, 1)).to(DEVICE)
+		self.target_zeros = torch.zeros((batch_size, 1)).to(DEVICE)
+
+	def generate_samples(self, latent_vec=None, num=None):
+		num = batch_size if num is None else num
+		latent_vec = create_noise(num) if latent_vec is None else latent_vec
+		with torch.no_grad():
+			samples = self.generator(latent_vec)
+		return samples
+
+	def train_step_generator(self):
+		self.generator.zero_grad()
+
+		latent_vec = create_noise(batch_size)
+		generated = self.generator(latent_vec)
+		classifications = self.discriminator(generated)
+		loss = self.criterion(classifications, self.target_ones)
+		loss.backward()
+		self.optim_g.step()
+		return loss.item()
+
+	def train_step_discriminator(self, images):
+		self.discriminator.zero_grad()
+
+		real_samples = images.to(DEVICE)
+		pred_real = self.discriminator(real_samples)
+		loss_real = self.criterion(pred_real, self.target_ones)
+
+		latent_vec = create_noise(batch_size)
+		with torch.no_grad():
+			fake_samples = self.generator(latent_vec)
+		pred_fake = self.discriminator(fake_samples)
+		loss_fake = self.criterion(pred_fake, self.target_zeros)
+
+		loss = (loss_real + loss_fake) /2
+		loss.backward()
+		self.optim_d.step()
+		return loss_real.item(), loss_fake.item()
+
+	def train_step(self, images):
+		loss_d = self.train_step_discriminator(images)
+		loss_g = self.train_step_generator()
+		return loss_g, loss_d
+
+#---------------------------------------------------------
+#---------------------------------------------------------
+#---------------------------------------------------------
+def create_noise(sample_size):
+	return torch.randn(sample_size, 100).to(DEVICE)
+
+generator = Generator(100).to(DEVICE)
+discriminator = Discriminator().to(DEVICE)
+
+gan = VanillaGAN(generator, discriminator)
+
+loss_g, loss_d_real, loss_d_fake = [], [], []
+start = time()
+for epoch in range(epochs):
+
+	loss_g_running, loss_d_real_running, loss_d_fake_running = 0, 0, 0
+
+	for i, images in enumerate(data_loader, 0):
+		lg_, (ldr_, ldf_) = gan.train_step(images[0])
+		loss_g_running += lg_
+		loss_d_real_running += ldr_
+		loss_d_fake_running += ldf_
+
+	loss_g.append(loss_g_running / batch_size)
+	loss_d_real.append(loss_d_real_running / batch_size)
+	loss_d_fake.append(loss_d_fake_running / batch_size)
+
+	print(f"Epoch {epoch+1}/{epochs} ({int(time() - start)}s):"
+			f" G={loss_g[-1]:.3f},"
+			f" Dr={loss_d_real[-1]:.3f},"
+			f" Df={loss_d_fake[-1]:.3f}")
+
+	if (epoch % 500 == 0) or ((epoch == epochs-1) and (i == len(dataloader)-1)):
+		with torch.no_grad():
+			fake = netG(fixed_noise).detach().cpu()
+		img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+
+generator.eval()
+torch.save(generator.state_dict(), 'model/generator.pth')
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+HTML(ani.to_jshtml())
 
