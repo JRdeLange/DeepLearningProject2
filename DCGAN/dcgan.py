83305c8d164e8a0c3429e7ac2542bec23282e637
MrShroom1988 <mrshroom1988@gmail.com>
2021-04-08 15:32:37 +0200

    cleaned up dcgan

--- DCGAN/dcgan.py
+++ DCGAN/dcgan.py
@@ -58,13 +58,13 @@ if __name__ == '__main__':
 	ndf = 64
 	
 	# Number of training epochs
-	num_epochs = 20
+	num_epochs = 200
 	
 	# Learning rate for optimizers
-	lr = 0.0002
+	lr = 0.0004
 	
 	# Beta1 hyperparam for Adam optimizers
-	beta1 = 0.5
+	beta1 = 0.75
 	
 	# Number of GPUs available. Use 0 for CPU mode.
 	ngpu = 1
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 	plt.figure(figsize=(8,8))
 	plt.axis("off")
 	plt.title("Training Images")
-	plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+	#plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
 	#plt.show()
 	
 	#---------------------------------------------------------
@@ -325,17 +325,17 @@ if __name__ == '__main__':
 	plt.xlabel("iterations")
 	plt.ylabel("Loss")
 	plt.legend()
-	plt.show()
+	#plt.show()
 
 	fig = plt.figure(figsize=(8,8))
 	plt.axis("off")
 	ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
 	ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-	plt.show()
+	#plt.show()
 
-	HTML(ani.to_jshtml())
+	#HTML(ani.to_jshtml())
 
 	plt.figure(figsize=(15,15))
 	plt.axis("off")
 	plt.imshow(np.transpose(img_list[-1],(1,2,0)))
-	plt.show()
+	plt.savefig("Results.png")
